c3e21f0a0f5cf6e1e170e31f81d8520ad5e5e65b
MrRoy09 <royketan09@gmail.com>
2024-01-03 23:30:22 +0530

    access both modes using terminal

--- fttmode.py
+++ fttmode.py
@@ -2,28 +2,13 @@ import pygame
 import wave
 import numpy as np
 import sys
-from scipy.fft import fft
+from scipy.fft import fft,fftfreq
 import matplotlib.pyplot as plt
-
-screen_width=1200
-screen_height=600
-fps=100
-number_bars=25
-with wave.open('song3.wav','rb') as audio_file:
-    channels,sample_width,framerate,nframes=audio_file.getparams()[0:4]
-    print(channels, sample_width)
-    data=np.frombuffer(audio_file.readframes(nframes),np.int32)
-
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('song3.wav')
-clock=pygame.time.Clock()
-window=pygame.display.set_mode((screen_width,screen_height))
-
-frames=framerate*1/fps
-print("approx number of frames is",frames)
+import os
 
 def signalProducer():
+    global start
+    global end
     current_frame = pygame.mixer.music.get_pos() * framerate // 1000
     signal=np.array(data[current_frame:current_frame+int(frames)])
     transformed=fft(signal)
@@ -39,16 +24,42 @@ def CreateRectangles(transformed):
         pygame.draw.rect(window,(255,0,0),rect_dim)
         pygame.draw.rect(window,(0,0,0),rect_dim,width=2)
 
+def init(status,filename):
+    global screen_height
+    global screen_width
+    global frames
+    global fps
+    global framerate
+    global data 
+    global window
 
-pygame.mixer.music.play()
-while True:
-    window.fill((0,0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.mixer.music.unload()
-            pygame.quit()
-            sys.exit()
-    signalProducer()
-    pygame.display.flip()
+    
+    if status==True:
+        screen_width=1200
+        screen_height=600
+        fps=100
+        with wave.open(filename,'rb') as audio_file:
+            channels,sample_width,framerate,nframes=audio_file.getparams()[0:4]
+            print(channels, sample_width)
+            data=np.frombuffer(audio_file.readframes(nframes),np.int32)
 
-    clock.tick(fps)
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(filename)
+        clock=pygame.time.Clock()
+        window=pygame.display.set_mode((screen_width,screen_height))
+
+        frames=framerate*1/fps
+        pygame.mixer.music.play()
+        while True:
+            window.fill((0,0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.mixer.music.unload()
+                    pygame.quit()
+                    os.remove('audio.wav')
+                    sys.exit()
+            signalProducer()
+            pygame.display.flip()
+            clock.tick(fps) 
+
--- visualizer.py
+++ visualizer.py
@@ -3,28 +3,7 @@ import wave
 import numpy as np
 import sys
 from scipy.fft import fft
-
-screen_width=1200
-screen_height=700
-fps=60
-number_bars=100
-white=(255,255,255)
-black=(0,0,0)
-current_color=black
-
-
-with wave.open('song3.wav','rb') as audio_file:
-    channels,sample_width,framerate,nframes=audio_file.getparams()[0:4]
-    print(channels, sample_width)
-    data=np.frombuffer(audio_file.readframes(nframes),np.int32)
-
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('song3.wav')
-clock=pygame.time.Clock()
-
-window=pygame.display.set_mode((screen_width,screen_height))
-frames=framerate*1/fps
+import os
 
 def CreateRectangles():
     current_frame = pygame.mixer.music.get_pos() * framerate // 1000
@@ -39,28 +18,56 @@ def CreateRectangles():
                 rectangle_dim=(i*rectangle_width,screen_height/2,rectangle_width,amplitude/(-2147483647 )*screen_height/3)
                 pygame.draw.rect(window,(255,0,0),rectangle_dim)
 
-pygame.mixer.music.play()
+def init(status,filename):
+    global framerate
+    global screen_width
+    global screen_height
+    global fps
+    global frames
+    global number_bars
+    global data 
+    global window 
 
-toggle_bg = pygame.Rect(1050, 10, 100, 30)
-while True:
-    window.fill(current_color)
-    CreateRectangles()
-    pygame.draw.rect(window, (233, 184, 36), toggle_bg)
-    font = pygame.font.Font(None, 28)
-    text = font.render("Toggle Bg", True, (0, 0, 0))
-    text_rect = text.get_rect(center=toggle_bg.center)
-    window.blit(text, text_rect)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.mixer.music.unload()
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if toggle_bg.collidepoint(event.pos):
-                current_color = black if current_color == white else white
-    pygame.display.flip()
+    screen_width=1200
+    screen_height=700
+    fps=60
+    number_bars=100
+    white=(255,255,255)
+    black=(0,0,0)
+    current_color=black
+    with wave.open(filename,'rb') as audio_file:
+        channels,sample_width,framerate,nframes=audio_file.getparams()[0:4]
+        data=np.frombuffer(audio_file.readframes(nframes),np.int32)
 
-    clock.tick(fps)
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    clock=pygame.time.Clock()
+    window=pygame.display.set_mode((screen_width,screen_height))
+    frames=framerate*1/fps
+    pygame.mixer.music.play()
+    toggle_bg = pygame.Rect(1050, 10, 100, 30)
+
+    while True:
+        window.fill(current_color)
+        CreateRectangles()
+        pygame.draw.rect(window, (233, 184, 36), toggle_bg)
+        font = pygame.font.Font(None, 28)
+        text = font.render("Toggle Bg", True, (0, 0, 0))
+        text_rect = text.get_rect(center=toggle_bg.center)
+        window.blit(text, text_rect)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.mixer.music.unload()
+                pygame.quit()
+                os.remove('audio.wav')
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if toggle_bg.collidepoint(event.pos):
+                    current_color = black if current_color == white else white
+        pygame.display.flip()
+
+        clock.tick(fps)
 
         
 
